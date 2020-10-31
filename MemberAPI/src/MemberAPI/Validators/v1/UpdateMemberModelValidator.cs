da6d7f790b9536bd1b2e52a347df434426ee7014
lijosebastian <weframes2019@gmail.com>
2020-10-31 17:12:04 +0400

    Added Validation for FirstName,LastName and Gender Field in UpdateMemberModel

--- MemberAPI/src/MemberAPI/Validators/v1/UpdateMemberModelValidator.cs
+++ MemberAPI/src/MemberAPI/Validators/v1/UpdateMemberModelValidator.cs
@@ -15,10 +15,20 @@ namespace MemberAPI.Validators.v1
             RuleFor(x => x.MemberId)
             .NotNull()
             .WithMessage("MemberId Cannot be Empty");
-              
+
+            RuleFor(x => x.FirstName)
+            .MinimumLength(2).
+            WithMessage("The first name must be at least 2 character long");
+
+            RuleFor(x => x.LastName)
+            .MinimumLength(2)
+            .WithMessage("The last name must be at least 2 character long");
+
+            RuleFor(x => x.Gender)
+            .IsEnumName(typeof(Gender))
+            .WithMessage("Gender Should be one among this :" + string.Join(",", Enum.GetNames(typeof(Gender))));
 
 
-        
 
 
         }
