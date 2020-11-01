3fa86f1a86e8764e44bdb3abd852d8b370d1b152
Lijo Sebastian <lsebastian@iris.local>
2020-11-01 13:30:34 +0400

    Added Test Project for Member

---
+++ MemberAPI/test/MemberAPI.Tests/MemberTest.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace MemberAPI.Tests
+{
+    public class MemberTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+        /*[Fact]
+        public void GetReturnsProduct()
+        {
+            // Arrange
+            var controller = new ProductsController(repository);
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var response = controller.Get(10);
+
+            // Assert
+            Product product;
+            Assert.IsTrue(response.TryGetContentValue<Product>(out product));
+            Assert.AreEqual(10, product.Id);
+        }
+        */
+
+       
+    }
+}
